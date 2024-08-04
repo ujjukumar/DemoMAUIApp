e7fafd853fc6bc0d121d2049ad20fa4967e0b386
Ujjawal Kumar <ujjawalkumar1810@gmail.com>
2024-08-04 12:13:12 +0530

    Add project files.

---
+++ DemoMAUIApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace DemoMAUIApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ DemoMAUIApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace DemoMAUIApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
